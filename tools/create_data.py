22eaea61facfac43543334bd763115e4552e9816
wzl <49808837+cbjszc@users.noreply.github.com>
2025-04-07 19:07:18 +0800

    Update create_data.py

--- tools/create_data.py
+++ tools/create_data.py
@@ -1,6 +1,5 @@
 import fire
 from det3d.datasets.nuscenes.nusc_common import create_nuscenes_infos 
-from det3d.datasets.waymo.waymo_convert import create_waymo_infos
 from create_gt_database import create_groundtruth_database
 
 
@@ -10,15 +9,6 @@ def nuscenes_data_prep(root_path, version="v1.0-trainval", nsweeps=10):
                                 root_path, 
                                 'infos_train_10sweeps_withvelo_filterZero.pkl',
                                 nsweeps=nsweeps)
-
-
-def waymo_data_prep(root_path, save_path, nsweeps=3):
-    create_waymo_infos(root_path, save_path)
-    create_groundtruth_database('WAYMO', 
-                                save_path, 
-                                'waymo_infos_train.pkl',
-                                nsweeps=nsweeps)
-
 
 if __name__ == '__main__':
     fire.Fire()
